140ab2bf37ce532605934936cff3837c96462ffc
awakeneddev <awakeneddevs@gmail.com>
2024-11-15 09:11:59 +0545

    video player - start and stop recording feature created

---
+++ src/VideoRecorder.tsx
@@ -0,0 +1,39 @@
+import { useState, useRef } from "react";
+
+export const VideoRecorder = () => {
+  const [permission, setPermission] = useState(false);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+
+  const getCameraPermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
+        setPermission(true);
+        setStream(streamData);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Video Recorder</h2>
+      <main>
+        <div className="video-controls">
+          {!permission ? (
+            <button onClick={getCameraPermission} type="button">
+              Get Camera
+            </button>
+          ) : null}
+          {permission ? <button type="button">Record</button> : null}
+        </div>
+      </main>
+    </div>
+  );
+};
